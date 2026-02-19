49cbcd1eb52a5f42261c259a7e0f1a9bf5c02d64
kalinkanya <copk-3@yandex.ru>
2026-02-19 09:32:24 +0300

    feat: add accordion functionality

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import { initBurgerMenu } from './js/burger-menu.js';
 import { initScrollDown } from './js/scroll-down.js';
+import { initAccordion } from './js/accordin.js';
+
 initBurgerMenu();
 initScrollDown();
+initAccordion();
 
